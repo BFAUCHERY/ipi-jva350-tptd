abc7d85bc88bda0aacab3dea70b6f2b5a6dc2e54
Laurent Thillou <badboboc69@gmail.com>
2022-11-10 16:18:30 +0100

    fix the method

--- src/main/java/com/ipi/jva350/model/Entreprise.java
+++ src/main/java/com/ipi/jva350/model/Entreprise.java
@@ -142,12 +142,12 @@ public final class Entreprise {
     }
 
     public static boolean estDansPlage(LocalDate d, LocalDate debut, LocalDate fin) {
-        if (d.isAfter(debut) && d.isBefore(fin)) {
+        if ((d.isAfter(debut) && d.isBefore(fin)) || d.isEqual(debut) || d.isEqual(fin)) {
             return true;
         } else {
             return false;
         }
-        // Comment : Les TDD ont permi de mettre en évidence que la date a tester peut être la date de debut ou la date de fin.
+        // Comment pour le TP : Les TDD ont permi de mettre en évidence que la date a tester peut être la date de debut ou la date de fin.
     }
 
 }
