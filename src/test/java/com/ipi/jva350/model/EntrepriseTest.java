2bb5b15e0ebde9cc5262e17f81afa56ada77909f
benoit.fauchery <Benoit.Fauchery@cgi.com>
2022-11-13 20:56:20 +0100

    TU1 - parametrisé

---
+++ src/test/java/com/ipi/jva350/model/EntrepriseTest.java
@@ -0,0 +1,31 @@
+package com.ipi.jva350.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class EntrepriseTest {
+
+	@ParameterizedTest(name = "Date {0}, expect ferié : {1}")
+	@CsvSource({
+	"'2022-11-10', 'false'",
+	"'2022-11-11', 'true'",
+	"'2022-11-12', 'false'",
+	"'2022-12-08', 'false'",
+	"'2022-01-01', 'true'",
+	"'2023-07-14', 'true'",
+	"'2023-04-10', 'true'",
+	"'2023-06-22', 'false'"
+	})
+	void testEstJourFerie(LocalDate date,boolean expectFerie) {
+		//Given
+		Entreprise entrepriseTest = new Entreprise();
+		//When, Then
+		Assertions.assertEquals(expectFerie,Entreprise.estJourFerie(date));
+	}
+
+}
