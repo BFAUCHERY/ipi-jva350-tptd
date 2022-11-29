263838bcb710e070054ed18159ce3d8b2bae46ca
Laurent Thillou <badboboc69@gmail.com>
2022-11-29 21:20:25 +0100

    delete unit test for other branch

---
+++ src/test/java/com/ipi/jva350/repository/SalarieAideADomicileRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.ipi.jva350.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SalarieAideADomicileRepositoryTest {
+
+	@Test
+	void testpartCongesPrisTotauxAnneeNMoins1() {
+		fail("Not yet implemented");
+		//Given
+		//When
+		//Then
+	}
+
+}
